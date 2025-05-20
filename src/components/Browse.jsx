e814df295ad26aa4533c1affc3887bf90bc2b170
Mahadev1729 <devcampus17@gmail.com>
2025-05-20 13:45:15 +0530

    re setup

---
+++ src/components/Browse.jsx
@@ -0,0 +1,9 @@
+
+const Browse=()=>{
+    return(
+        <>
+        <h> hello welcome to broswe page</h>
+        </>
+    )
+}
+export default Browse;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () =>{
+   return (
+    <div className="absolute px-8 py-8 bg-gradient-to-b from from-black z-10">
+           <img className="w-80"
+             src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"
+             alt="Logo"
+           />
+    </div>
+   )
+}
+
+export default Header;
