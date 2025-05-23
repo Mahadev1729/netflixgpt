f7c26fea365c6e81620cff23240f18723e5cd2ef
Mahadev1729 <devcampus17@gmail.com>
2025-05-23 16:48:40 +0530

    create error page unmatched routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,16 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 const Body = () => {
   const appRouter = createBrowserRouter([
     {
       path: "/",
       element: <Login />,
+      errorElement:<ErrorPage/>
     },
     {
       path: "/browse",
       element: <Browse />,
+      errorElement:<ErrorPage/>,
     },
   ]);
 
