a0aeebd7086ca54a78c66d6c1692d8c17dedbea8
Mahadev1729 <devcampus17@gmail.com>
2025-05-24 12:34:54 +0530

    add to store

--- src/utilis/appStore.jsx
+++ src/utilis/appStore.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import moviesReducers  from "./movieSlice"
 
 const appStore=configureStore(
     {
         reducer:{
             user:userReducer,
+            movies:moviesReducers,
         }
     }
 )
