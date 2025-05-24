e6bbd022dfeb1c7ad4fbe51372388baf531684a5
Mahadev1729 <devcampus17@gmail.com>
2025-05-24 21:42:07 +0530

    format the code

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -2,27 +2,26 @@
 import { API_OPTIONS } from "../utilis/constant";
 
 const VideoBackground = ({ movieId }) => {
-  // fetch trailer video
+   // fetch trailer video
 
-  const getMovieVideo = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/950387/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
+   const getMovieVideo = async () => {
+      const data = await fetch(
+         "https://api.themoviedb.org/3/movie/950387/videos?language=en-US",
+         API_OPTIONS
+      );
+      const json = await data.json();
+      console.log(json);
 
-    const filterData = json.results.filter((video) => video.type == "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    console.log(filterData);
-  };
-  useEffect(() => {
-    getMovieVideo();
-  }, []);
+      const filterData = json.results.filter((video) => video.type == "Trailer");
+      const trailer = filterData.length ? filterData[0] : json.results[0];
+      console.log(trailer);
+      console.log(filterData);
+   };
+   useEffect(() => {
+      getMovieVideo();
+   }, []);
 
-  return <div></div>;
+   return <div></div>;
 };
-
 
 export default VideoBackground;
