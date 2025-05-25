cc6c14583e0e773ab8741e1ea9183d921f737700
Mahadev1729 <devcampus17@gmail.com>
2025-05-25 12:32:24 +0530

    format

--- src/hooks/useMovieTrailer.jsx
+++ src/hooks/useMovieTrailer.jsx
@@ -4,28 +4,28 @@ import { API_OPTIONS } from "../utilis/constant";
 
 import { addTrailerVideo } from "../utilis/movieSlice";
 
-const useMovieTrailer=(movieId)=>{
-    const dispatch = useDispatch();
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
 
-    const getMovieVideo = async () => {
-      const data = await fetch(
-        "https://api.themoviedb.org/3/movie/"+movieId+"/videos?language=en-US",
-        API_OPTIONS
-      );
-      const json = await data.json();
-      console.log(json);
+  const getMovieVideo = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
 
-      const filterData = json.results.filter(
-        (video) => video.type == "Trailer"
-      );
-      const trailer = filterData.length ? filterData[0] : json.results[0];
-      console.log(trailer);
-      console.log(filterData);
-      dispatch(addTrailerVideo(trailer));
-    };
-    useEffect(() => {
-      getMovieVideo();
-    }, []);
-}
+    const filterData = json.results.filter((video) => video.type == "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    console.log(trailer);
+    console.log(filterData);
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    getMovieVideo();
+  }, []);
+};
 
 export default useMovieTrailer;
