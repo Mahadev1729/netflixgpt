a2db4d8d0421e6358ccd358ccbceaf9af96f89a9
Mahadev1729 <devcampus17@gmail.com>
2025-05-26 10:18:00 +0530

    try to build the movielist and moviecard

--- src/components/SecondarayContainer.jsx
+++ src/components/SecondarayContainer.jsx
@@ -1,8 +1,12 @@
-﻿
+﻿import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
+
 
 const SecandaryContainer=()=>{
+    const movies=useSelector(store=>store.movies);
     return(
         <div>
+            <MovieList  title={"Now Playing"} movies={movies.nowPlayingMovies}/>
             {/**
              Multiple movie list
              trending Movies
