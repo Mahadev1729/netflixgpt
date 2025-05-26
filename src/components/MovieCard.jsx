a758668154f8a51b9f06b4ad96836386d87380ae
Mahadev1729 <devcampus17@gmail.com>
2025-05-26 10:26:00 +0530

    format

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,10 @@
 ﻿import { IMG_CDN_URL } from "../utilis/constant";
 
-const MovieCard=()=>{
+const MovieCard = () => {
     return (
         <div>
-            <img alt="Movie Card"
-            src={IMG_CDN_URL}/>
+            <img alt="Movie Card" src={IMG_CDN_URL} />
         </div>
-    )
-}
+    );
+};
 export default MovieCard;
