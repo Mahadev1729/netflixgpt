cb91d16a6152575dbaee1ce265ca9f4925352e18
Mahadev1729 <devcampus17@gmail.com>
2025-05-26 13:33:54 +0530

    adjust the movie card size

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,15 @@
 ﻿import { IMG_CDN_URL } from "../utilis/constant";
 
-const MovieCard = () => {
-    return (
-        <div>
-            <img alt="Movie Card" src={IMG_CDN_URL} />
-        </div>
-    );
+const MovieCard = ({ posterPath }) => {
+  return (
+    <div className="min-w-[280px] h-[300px] rounded-lg overflow-hidden shadow-lg">
+      <img
+        className="w-full h-full object-cover"
+        alt="Movie Card"
+        src={IMG_CDN_URL + posterPath}
+      />
+    </div>
+  );
 };
+
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,18 @@
 ﻿import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
-    if(!movies || movies.length===0){
-        return <div>No movies display</div>
-    }
+  if (!movies || movies.length === 0) {
+    return <div>No movies to display</div>;
+  }
+
   return (
-    <div>
-      <div>
-        <h1>{title}</h1>
-        <div>
-            {movies.map(movie=><MovieCard key={movie.id} posterPath={movie.poster_path}/>)}
+    <div className="my-4">
+      <h1 className="text-xl font-semibold mb-2">{title}</h1>
+      <div className="flex overflow-x-scroll h-[350px] w-full">
+        <div className="flex gap-3">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+          ))}
         </div>
       </div>
     </div>
