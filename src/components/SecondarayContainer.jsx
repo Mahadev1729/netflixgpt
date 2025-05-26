ef5a95ea0b8731aba0beb5af46dd80cd31101e2a
Mahadev1729 <devcampus17@gmail.com>
2025-05-26 18:01:13 +0530

    some modification

--- src/components/SecondarayContainer.jsx
+++ src/components/SecondarayContainer.jsx
@@ -8,9 +8,9 @@ const SecandaryContainer = () => {
       <div className=" bg-black">
         <div className="-mt-60 pl-12 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.popularMovies} />
           <MovieList title={"Upcoming Movies"} movies={movies.upcomingMovies} />
           <MovieList title={"Top Rated"} movies={movies.topratedMovies} />
+          <MovieList title={"Popular"} movies={movies.popularMovies} />
         </div>
       </div>
     )
