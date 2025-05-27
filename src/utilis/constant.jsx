5adbf52433b285d000812dfe0ee04d39ca90e1bb
Mahadev1729 <devcampus17@gmail.com>
2025-05-27 13:38:49 +0530

    reduce the code

--- src/utilis/constant.jsx
+++ src/utilis/constant.jsx
@@ -1,53 +1,32 @@
-﻿export const LOGO_URL =
-  "https://www.pngall.com/wp-content/uploads/13/Netflix-Logo-PNG-Clipart.png";
-export const USER_AVATAR =
-  "https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg";
-export const BACKGROUND_IMAGE =
-  "https://www.teknofilo.com/wp-content/uploads/2021/06/Netflix-1280x720.jpg";
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTY4OWZiZDBlNGY4YjcwNGQxNzYzM2I1N2MxY2UwNyIsIm5iZiI6MTc0ODA2NzI5Ni40MTgsInN1YiI6IjY4MzE2M2UwNmJiNzJmOTg2NDA1NDhkNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QdbD9B4WGk2HIvVt9suBWGO2t3voR_7eeW0BkhWE0Nc",
-  },
-};
-
-export const IMG_CDN_URL =
-  "https://image.tmdb.org/t/p/w780";
-  
-  export const SUPPORTED_LANGUAGES = [
-    { identifier: "en", name: "English" },
-    { identifier: "hi", name: "Hindi" },
-    { identifier: "es", name: "Spanish" },
-    { identifier: "fr", name: "French" },
-    { identifier: "de", name: "German" },
-    { identifier: "zh", name: "Chinese" },
-    { identifier: "ja", name: "Japanese" },
-    { identifier: "ru", name: "Russian" },
-    { identifier: "ar", name: "Arabic" },
-    { identifier: "pt", name: "Portuguese" },
-    { identifier: "bn", name: "Bengali" },
-    { identifier: "pa", name: "Punjabi" },
-    { identifier: "ko", name: "Korean" },
-    { identifier: "it", name: "Italian" },
-    { identifier: "ur", name: "Urdu" },
-    { identifier: "ta", name: "Tamil" },
-    { identifier: "te", name: "Telugu" },
-    { identifier: "mr", name: "Marathi" },
-    { identifier: "tr", name: "Turkish" },
-    { identifier: "vi", name: "Vietnamese" },
-    { identifier: "gu", name: "Gujarati" },
-    { identifier: "pl", name: "Polish" },
-    { identifier: "uk", name: "Ukrainian" },
-    { identifier: "fa", name: "Persian" },
-    { identifier: "ro", name: "Romanian" },
-    { identifier: "nl", name: "Dutch" },
-    { identifier: "th", name: "Thai" },
-    { identifier: "sv", name: "Swedish" },
-    { identifier: "id", name: "Indonesian" },
-    { identifier: "ms", name: "Malay" },
-  ];
-  
-  
-  
+﻿export const SUPPORTED_LANGUAGES = [
+  ["en", "English"],
+  ["hi", "Hindi"],
+  ["es", "Spanish"],
+  ["fr", "French"],
+  ["de", "German"],
+  ["zh", "Chinese"],
+  ["ja", "Japanese"],
+  ["ru", "Russian"],
+  ["ar", "Arabic"],
+  ["pt", "Portuguese"],
+  ["bn", "Bengali"],
+  ["pa", "Punjabi"],
+  ["ko", "Korean"],
+  ["it", "Italian"],
+  ["ur", "Urdu"],
+  ["ta", "Tamil"],
+  ["te", "Telugu"],
+  ["mr", "Marathi"],
+  ["tr", "Turkish"],
+  ["vi", "Vietnamese"],
+  ["gu", "Gujarati"],
+  ["pl", "Polish"],
+  ["uk", "Ukrainian"],
+  ["fa", "Persian"],
+  ["ro", "Romanian"],
+  ["nl", "Dutch"],
+  ["th", "Thai"],
+  ["sv", "Swedish"],
+  ["id", "Indonesian"],
+  ["ms", "Malay"],
+].map(([identifier, name]) => ({ identifier, name }));
