87d8517dd43bcd143c60a8cf0ac9a9b7ed57d593
Mahadev1729 <devcampus17@gmail.com>
2025-05-27 14:08:50 +0530

    show when gpt search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utilis/userSlice";
-import { LOGO_URL, USER_AVATAR,SUPPORTED_LANGUAGES } from "../utilis/constant";
+import { LOGO_URL, USER_AVATAR, SUPPORTED_LANGUAGES } from "../utilis/constant";
 import { toggleGptSearchView } from "../utilis/gptSlice";
 import { changeLanguage } from "../utilis/configSlice";
 
@@ -15,6 +15,8 @@ const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -22,7 +24,6 @@ const Header = () => {
       })
       .catch((error) => {
         // An error happened.
-       
       });
   };
   useEffect(() => {
@@ -49,31 +50,31 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
-
-  const handleGptSearchClick=()=>{
-       // toggle the button
-       dispatch(toggleGptSearchView())
-
+  const handleGptSearchClick = () => {
+    // toggle the button
+    dispatch(toggleGptSearchView());
   };
 
-  const handleLanguageChange=(e)=>{
-     dispatch(changeLanguage(e.target.value))
-  }
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="w-full absolute px-8 py-8 bg-gradient-to-b from from-black z-10 flex justify-between">
       <img className="w-80" src={LOGO_URL} alt="Logo" />
       {user && (
         <div className="w-full flex justify-end p-4">
-          <select
-          onChange={handleLanguageChange}
-            className="p-2 m-2 bg-cyan-500 text-white"
+          {showGptSearch && (
+            <select
+              onChange={handleLanguageChange}
+              className="p-2 m-2 bg-cyan-500 text-white"
             >
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option key={lang.identifier} value={lang.identifier}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
           <button
             onClick={handleGptSearchClick}
             className=" bg-cyan-500 hover:bg-cyan-600 text-black font-semibold py-2 px-4 mx-4 my-2 rounded-lg shadow transition duration-200"
